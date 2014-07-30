6300b8f76ff97dafccc2096b4c7414408da5862e
Seth Geyer <seth.geyer@gmail.com>
2014-07-30 10:05:22 -0600

    count instances refactoring

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,17 +1,16 @@
 require 'spec_helper'
 
 describe Rectangle do
+  after(:each) do
+    Rectangle.count = 0
+  end
+
   let(:rectangle) { Rectangle.new(4,5) }
 
   it "returns a length and width upon initialization" do
-    expect(Rectangle.count).to eq(0)
     expect(rectangle.length).to eq(4)
     expect(rectangle.width).to eq(5)
-    expect(Rectangle.count).to eq(1)
-    Rectangle.new(5,6)
-    expect(Rectangle.count).to eq(2)
   end
-
 
   describe "#area" do
     it "returns the area" do
@@ -19,10 +18,18 @@ describe Rectangle do
     end
   end
 
-
   describe "#perimeter" do
     it "returns the perimeter" do
       expect(rectangle.perimeter).to eq(18)
+    end
+  end
+
+  describe "#self.count" do
+    it "counts the number of objects created" do
+      rectangle
+      expect(Rectangle.count).to eq(1)
+      Rectangle.new(5,6)
+      expect(Rectangle.count).to eq(2)
     end
   end
 
